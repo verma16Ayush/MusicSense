a0f1192dd27399582a353bc4a08a95897cfdae88
Ayush <verma16.aysuh@gmail.com>
2022-11-14 16:37:42 +0530

    wrote some db handling code.

--- hello.py
+++ hello.py
@@ -1,19 +1,53 @@
 import mslib.db_handler.handler
 from mslib.recognizer.decoder import *
 from mslib.recognizer import fingerprint
+import time
 
+db = mslib.db_handler.handler.MySQLDB()
+# db.reset_tables()
+
+num_songs = 0
+total_hashes_inserted = 0
+
+start_time = time.process_time()
 for file in find_files('./data/', ['mp3', '.wav']):
+    num_songs += 1
     data = read(file[0])
     if data is not None:
         channels, sample_rate, hash_ = data
         song_name = get_audio_name_from_path(file[0])
-        fingerprint.fingerprint(channel_samples=channels[0], song_name=song_name)
+        fingerprints = fingerprint.fingerprint(channel_samples=channels[0], song_name=song_name)
+
         print(f'''
+        ******** processing file ***********
         file_name: { song_name }
         file_format: {file[1]}
         no_of_channels: {len(channels)}
         sample_rate: {sample_rate},
         no_of_samples: {len(channels[0])}
         file_hash: {hash_}
+        num_hashes: {len(fingerprints)}
         -----------------------------------
         ''')
+
+        song_id = db.insert_song(song_name, hash_, len(fingerprints))
+
+        for fp in fingerprints:
+            db.insert_fingerprint(fp[0], song_id, fp[1])
+
+        total_hashes_inserted += len(fingerprints)
+
+
+time_taken = time.process_time() - start_time
+
+print(f'''
+*********************************************************
+*                      SUMMARY                          *
+*********************************************************
+
+* total songs processed: {num_songs}
+* total fingerprints inserted: {total_hashes_inserted}
+* time taken: {time_taken}
+
+*********************************************************
+''')
--- mslib/db_handler/handler.py
+++ mslib/db_handler/handler.py
@@ -1,7 +1,64 @@
 from .connect import my_db
+from .query_literals import *
+
 
 class MySQLDB():
 
     def __init__(self):
-        pass
+        """
+        initialises the DB connection and a cursor to execute queries
+        """
+        self.cursor = my_db.cursor()
+        self.cursor.execute(CREATE_SONGS_TABLE)
+        self.cursor.execute(CREATE_FINGERPRINTS_TABLE)
+        self.cursor.execute(DELETE_UNFINGERPRINTED)
+
+    def insert_fingerprint(self, fingerprint: str, song_id: int, offset: int):
+        """
+        inserts a single fingerprint into the db. Had to explicitly typecast
+        offset (a numpy.int64) into a python int. wasn't sure where to do it so here it is
+
+        :param fingerprint: the sha1 hash we need to enter the DB
+        :param song_id: the song id of the song to wich the hash belongs to
+        :param offset: the offset of the anchor peak that was used to generate the hash
+        """
+        self.cursor.execute(INSERT_FINGERPRINT, (fingerprint, song_id, int(offset)))
+
+    def insert_song(self, song_name: str, file_hash: str, num_hashes: int) -> int:
+        '''
+        inserts a song into the db
+
+        :param song_name: name of the song file
+        :param file_hash: file hash of the song
+        :param num_hashes: number of hashes generated for that song
+        '''
+        self.cursor.execute(INSERT_SONG, (song_name, file_hash, num_hashes))
+        return self.cursor.lastrowid
+
+    def clear_db(self):
+        """
+        clears db and deletes fingerprints and songs tables
+        """
+        self.cursor.execute(DELETE_ALL_FINGERPRINTS)
+        self.cursor.execute(DELETE_ALL_SONGS)
+        self.cursor.execute(DROP_FINGERPRINTS)
+        self.cursor.execute(DROP_SONGS)
+
+    def delete_unfingerprinted(self):
+        """
+        deletes fields in songs table which are not fingerprinted
+        """
+        self.cursor.execute(DELETE_UNFINGERPRINTED)
     
+    def reset_tables(self):
+        """
+        deletes all entries from both songs and fingerprints tables.
+        ** USE WITH CAUTION **
+        """
+        self.cursor.execute(DELETE_ALL_FINGERPRINTS)
+        self.cursor.execute(DELETE_ALL_SONGS)
+
+
+    def __exit__(self):
+        self.cursor.close()
+        my_db.close()
